672a2ef02a63576beaff9965ad16551b8247d4e3
thanhlazio <thanhlh@lifetimetech.vn>
2018-09-02 00:25:34 +0700

    Load all data or display last cell

---
+++ SmoothenTableView/Cell/CoinTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CoinTableViewCell.swift
+//  SmoothenTableView
+//
+//  Created by MINATO on 9/1/18.
+//  Copyright © 2018 MINATO. All rights reserved.
+//
+
+import UIKit
+
+class CoinTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SmoothenTableView/Model/Coin.swift
@@ -0,0 +1,9 @@
+//
+//  Coin.swift
+//  SmoothenTableView
+//
+//  Created by MINATO on 9/1/18.
+//  Copyright © 2018 MINATO. All rights reserved.
+//
+
+import Foundation
